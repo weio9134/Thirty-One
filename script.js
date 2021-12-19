ee1971a3fd419601977e73d9bfbaf7eedd43fa35
weio9134 <96361986+weio9134@users.noreply.github.com>
2021-12-18 22:25:04 -0500

    Added files
    Moved all files to this directory

---
+++ script.js
@@ -0,0 +1,93 @@
+class Card 
+{
+    constructor(suit, value) 
+    {
+        switch(suit)
+        {
+            case 1: this.Suit = 'clubs'; break;
+            case 2: this.Suit = 'diamonds'; break;
+            case 3: this.Suit = 'hearts'; break;
+            case 4: this.Suit = 'spades'; break;
+        }
+
+        this.Name = value + '_of_' + this.Suit + '.png';
+
+        if(value === 1)
+            this.Value = 11;
+        else if(value >= 10)
+            this.Value = 10;
+        else
+            this.Value = value;
+    }
+    get suit()
+    {
+        return this.Suit;
+    }
+    get value()
+    {
+        return this.Value;
+    }
+    get name()
+    {
+        return this.Name;
+    }
+}
+
+const BOARD = document.getElementById('board');
+
+function initialize()
+{
+    // INITIALIZE A DECK OF 52 CARDS
+    deck = [];
+    for(var i = 1; i < 5; i++)
+        for(var j = 1; j < 14; j++)
+            deck.push(new Card(i, j));
+    deck.sort(() => 0.5-Math.random());
+    startGame();
+}
+
+function startGame()
+{
+    // GIVES EVERYONE THEIR HAND
+    mrCaution = [];
+    msAggressive = [];
+    mrClueless= [];
+    player = [];
+    discard = [deck[0]];
+    deck.shift();
+
+    for(var i = 0; i < 3; i++)
+    {
+        mrCaution.push(deck[0]);
+        deck.shift();
+        msAggressive.push(deck[0]);
+        deck.shift();
+        mrClueless.push(deck[0]);
+        deck.shift();
+        player.push(deck[0]);
+        deck.shift();
+    }
+    addCardToHand('player', player);
+    addCardToHand('caution', mrCaution);
+    addCardToHand('aggresive', msAggressive);
+    addCardToHand('clueless', mrClueless);
+
+    startRound();
+}
+
+function addCardToHand(person, arr)
+{
+    player = BOARD.getElementsByTagName('div')[person];
+    arr.forEach(card => {
+        var elem = document.createElement('img');
+        if(person == 'player')
+            elem.src = 'cards/' + card.name;
+        else
+            elem.src = 'card_back.png'
+        player.appendChild(elem);
+    });
+}
+
+function startRound()
+{
+}
